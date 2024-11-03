9b7ed9c195ef01b6f86996cfc03d2e8e3ae79b34
Nam Duong <phamnamduong583@gmail.com>
2024-11-03 16:45:35 +0700

    catch all routes

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { page } from "$app/stores";
+  console.log();
+
+  const slugArr = $page.params.slug.split("/");
+</script>
+
+<h1>Docs homepage</h1>
+{#if slugArr.length === 1}
+  <h1>Viewing docs for feature {slugArr[0]}</h1>
+{:else if slugArr.length === 2}
+  <h1>Viewing docs for sub-feature {slugArr[0]} - {slugArr[1]}</h1>
+{/if}
